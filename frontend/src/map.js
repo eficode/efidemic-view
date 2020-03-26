9e9391138c40f68e18e2c185bb857bd1967c60a7
reino <reino.pesonen@eficode.com>
2020-03-26 14:27:57 +0200

    Multiple markkers and some changes to sub package for better security

--- frontend/src/map.js
+++ frontend/src/map.js
@@ -1,29 +1,58 @@
 import React from 'react';
+import * as Nominatim from 'nominatim-browser';
 import { Map, TileLayer, CircleMarker, Popup }  from 'react-leaflet'
 
 class LMap extends React.Component {
   constructor() {
     super()
     this.state = {
-      lat: 61.9241,
-      lng: 25.7482,
+      // Finland location (default)
+      lat: 63.2467777,
+      lng: 25.9209164,
       zoom: 6
     }
   }
 
-  render() {
+  componentDidMount() {
+    Nominatim.geocode({
+      q: "33450 Finland",
+      addressdetails: true
+    })
+    .then((results) =>
+    {
+      var result = results[0];
+      this.setState({ lat: result.lat, lng: result.lon })
+      // console.log(result.lat);
+      // console.log(result.lon);
+      // console.log(result.display_name);
+
+    })
+    .catch((error) =>
+    {
+        console.error(error);
+    });
+}
+
+  render() { 
     const position = [this.state.lat, this.state.lng];
+    var countryCenter = [63.2467777, 25.9209164] // Finland
     return (
-      <Map center={position} zoom={this.state.zoom}>
+      <Map center={countryCenter} zoom={this.state.zoom}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
         />
-        <CircleMarker center={position} radius='10' attribution='jotain' color="red">
+        {[position, countryCenter].map(location => (
+          <CircleMarker center={[
+            location[0],
+            location[1]
+          ]} radius='10' color='yellow'>
           <Popup>
             Many sick here is...
           </Popup>
         </CircleMarker>
+        ))}
+        
       </Map>
     );
   }
